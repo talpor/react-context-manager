17770584e898aa63d9c3b6ef42210c22a58faf20
Juan Perozo <jperozo@talpor.com>
2020-01-24 00:58:42 -0300

    fixing scope issue

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
 import React, { useReducer } from 'react';
 
-import { injectCallback } from './binding';
 import mapContextToProps from './hoc';
 import { getContext } from './hooks';
 import createContext from './manager';
+import { injectCallback } from './utils';
 
 const contextManager = { version: '0.0.1', Contexts: {} };
 
-const binding = (action) => (args) => action(args);
-const dispatching = (dispatch) => (action) => (args) => dispatch([action, args]);
-// Fix scope issue
+const dispatching = (dispatch) => (scope) => (action) => (args) => dispatch([scope, action, args]);
 const reducer = (state, args) => {
-  const [func, ...funcArgs] = args;
-  return { ...state, ...func(state, funcArgs)}
+  const [scope, func, ...funcArgs] = args;
+  return { ...state, [scope]: {...func(state, ...funcArgs)} };
 };
+const binding = (_) => (action) => (...args) => action(...args);
 
 const ContextProvider = ({ store, actions, children, name = 'root' }) => {
-  const boundActions = injectCallback(actions, binding);
   const [state, dispatcher] = useReducer(reducer, store);
-  const Context = createContext(contextManager)(state, dispatcher, name);
+  const boundActions = injectCallback(actions, binding);
   const dispatcherActions = injectCallback(boundActions, dispatching(dispatcher));
+  const Context = createContext(contextManager)(state, dispatcherActions, name);
   const value = {
     actions: dispatcherActions,
     ...state
