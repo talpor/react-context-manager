659533dfcfdfdd56d55121b344eee81c61deb40a
Max Rondon <mrondon@talpor.com>
2020-02-10 16:15:13 -0300

    removed deprecated dependencies and configured jest html reporter

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  "testPathIgnorePatterns": [
+    "build/"
+  ],
+  "reporters": [
+    "default",
+    ["./node_modules/jest-html-reporter", {
+      "pageTitle": "Test Report",
+      "outputPath": "coverage/index.html"
+    }]
+  ]
 };
